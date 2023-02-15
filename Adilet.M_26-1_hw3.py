76c2245899ae5be428bffba62536d9bdb486419e
Adilet-Mederov <amederov777@gmail.com>
2023-02-15 22:04:34 +0600

    Firts commit

--- Adilet.M_26-1_hw3.py
+++ Adilet.M_26-1_hw3.py
@@ -16,7 +16,22 @@ TOKEN=config('TOKEN')
 bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
+@db.message_handler(commands=['start' , 'hello'])
+async def start_handler(massage:types.message):
+    await bot.send_message(massage.from_user.id,f'{massage.from_user.first_name}')
+    await massage.answer('Привет, это бот!')
+    # await massage.reply(massage.from_user.first_name)
 
-if __name__=='__main__':
+
+
+@db.message_handler()
+async def echo(massege:types.Message):
+    await bot.send_message(massege.from_user.id,massege.text)
+    await massege.answer('На этом всё!')
+
+
+
+
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(db, skip_updates=True)
